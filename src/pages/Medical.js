5c71570411de61e5118b0ca9498db5cdaf03d827
Rens Junior Sibarani <garoga.sehat@gmail.com>
2023-05-09 10:06:58 +0700

    update

--- src/pages/Medical.js
+++ src/pages/Medical.js
@@ -1,15 +1,29 @@
 import Layout from "../components/Sidebar/Layouts/Layout";
-
+import "./medical.css"
 const Medical = () => {
   return (
-    <Layout>
-      <h1>MEDICAL</h1>
-      <p>Testing</p>
-      <p>Testing</p>
-      <p>Testing</p>
-      <p>Testing</p>
-    </Layout>
-  );
+    <div className="Apps">
+      <Layout/>
+      <div className="main">
+      <div style={{
+        borderRadius:"20px",
+        backgroundColor: "#FFFFFF",
+        marginTop:"50px",
+        padding:"20px"
+       }}>
+        <h3 >Data Diri Pasien</h3>
+        <hr style={{
+          color:"black",
+          width:"97%",
+          margin:"0 auto"
+        }}/>
+          <h5>Nama : </h5>
+          <h5>Tanggal Lahir : </h5>
+          <h5>Alamat : </h5>
+       </div>
+      </div>
+    </div>
+  );
 };
 
 export default Medical;
